43ed9ca8f2ebab45323aa71ba04c549aa221437a
Yan Zingra Pereira <yan.zingra12@gmail.com>
2024-01-23 18:18:53 -0800

    bug fix test

---
+++ src/test/java/azure/trainning/ci/AzureTranningCIApplicationTest.java
@@ -0,0 +1,14 @@
+package azure.trainning.ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AzureTranningCIApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
