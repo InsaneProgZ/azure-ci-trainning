d5ecf77d58981eb2e3e534949a2aaa2a3fd087de
Yan Zingra Pereira <yan.zingra12@gmail.com>
2024-01-23 17:55:58 -0800

    first commit

---
+++ src/main/java/TrainningCIAzure/azurecicd/AzureCiCdApplication.java
@@ -0,0 +1,13 @@
+package TrainningCIAzure.azurecicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TrainningCIAzure/azurecicd/AzureCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package TrainningCIAzure.azurecicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
